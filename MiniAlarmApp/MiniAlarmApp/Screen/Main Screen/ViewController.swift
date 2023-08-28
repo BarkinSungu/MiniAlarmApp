cd12fc91ec35f835cd67ddd629ab240569ad4077
BarkinSungu <barkinsungu@gmail.com>
2023-08-28 20:00:41 +0300

    Push notification added successfully.

--- MiniAlarmApp/MiniAlarmApp/Screen/Main Screen/ViewController.swift
+++ MiniAlarmApp/MiniAlarmApp/Screen/Main Screen/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        scheduleNotification(at: Date().addingTimeInterval(60))
     }
-
-
+    
+    func scheduleNotification(at date: Date) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if granted {
+                let content = UNMutableNotificationContent()
+                content.title = "Wake up!"
+                content.body = "Time to wake up!"
+                content.sound = .default
+                
+                let triggerDate = Calendar.current.dateComponents([.hour, .minute], from: date)
+                let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    if let error = error {
+                        print("Error scheduling notification: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
+    }
 }
-
